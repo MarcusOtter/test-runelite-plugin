62096544f4ff651a242eb90416437e5bfef070b9
MarcusOtter <github@otterstrom.dev>
2024-03-20 01:55:49 +0100

    Play C Engineer sound when logging in

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -2,6 +2,10 @@ package com.example;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
@@ -11,6 +15,10 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Slf4j
 @PluginDescriptor(
@@ -23,6 +31,11 @@ public class ExamplePlugin extends Plugin
 
 	@Inject
 	private ExampleConfig config;
+
+	@Inject
+	private ScheduledExecutorService executor;
+
+	Clip clip = null;
 
 	@Override
 	protected void startUp() throws Exception
@@ -42,6 +55,30 @@ public class ExamplePlugin extends Plugin
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
 			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+
+			executor.submit(() -> {
+				try (var resourceStream = ExamplePlugin.class.getResourceAsStream("/DyingHCIMCompleted_r1.wav")) {
+					if (resourceStream == null) {
+						log.error("Failed to load C Engineer sound as resource stream was null!");
+					} else {
+						InputStream fileStream = new BufferedInputStream(resourceStream);
+						AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(fileStream);
+						if (clip == null) {
+							clip = AudioSystem.getClip();
+						}
+						clip.open(audioInputStream);
+						clip.start();
+						log.info("C Engineer sound loaded and played!");
+
+						// Stop the clip after 500ms:
+						// Thread.sleep(500);
+						// clip.stop();
+					}
+				}
+				catch (Exception e) {
+					log.error(e.getMessage());
+				}
+			});
 		}
 	}
 
